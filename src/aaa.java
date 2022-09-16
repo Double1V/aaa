690f5d94974fd28eec3ebb87ad6254dfa89f386e
Double1V <113297720+Double1V@users.noreply.github.com>
2022-09-16 15:48:37 +0300

    add file

---
+++ src/aaa.java
@@ -0,0 +1,9 @@
+public class aaa {
+
+    public static String solution(String str) {
+        StringBuilder sb=new StringBuilder(str);
+        sb.reverse();
+        return sb.toString();
+    }
+
+}
